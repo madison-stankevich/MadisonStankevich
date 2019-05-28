d93febb8c2448617620504283592ed46e8330ea3
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-05-28 12:44:32 -0700

    added NavBar menu; functional. No hamburger image yet.

--- src/Containers/NavBar.js
+++ src/Containers/NavBar.js
@@ -5,22 +5,40 @@ import { Link} from 'react-router-dom';
 class NavBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            menuDisplay: false
+         }
     }
+
+    handleClick = () => {
+        this.setState((prevState) => {
+            return {menuDisplay: !prevState.menuDisplay};
+        })
+    }
+
     render() { 
         return ( 
             <div>
                 NAVBAR.
-                <br/>
-                <Link className="" to="/home">Home</Link>
-                <br/>
-                <Link className="" to="/projects">Projects</Link>
-                <br/>
-                <Link className="" to="/blog">Blog</Link>
-                <br/>
-                <Link className="" to="/about">About</Link>
-                <br/>
-                <Link className="" to="/contact">Contact</Link>
+                {this.state.menuDisplay == false ?
+                    <div>
+                        <button onClick={this.handleClick}>Menu</button>
+                    </div>
+                    :
+                    <div>
+                        <button onClick={this.handleClick}>Menu</button>
+                        <br/>
+                        <Link className="" onClick={this.handleClick} to="/home">Home</Link>
+                        <br/>
+                        <Link className="" onClick={this.handleClick} to="/projects">Projects</Link>
+                        <br/>
+                        <Link className="" onClick={this.handleClick} to="/blog">Blog</Link>
+                        <br/>
+                        <Link className="" onClick={this.handleClick} to="/about">About</Link>
+                        <br/>
+                        <Link className="" onClick={this.handleClick} to="/contact">Contact</Link>
+                    </div>
+                }
             </div>
          );
     }
