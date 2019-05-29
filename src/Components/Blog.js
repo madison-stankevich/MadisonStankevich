11531a1801ffab7c8e6b961328c1b7f728d37471
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-05-29 14:27:30 -0700

    added blog entry.

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -20,6 +20,25 @@ class Blog extends Component {
                     For my day-to-day microblogging journey, follow me on {" "}
                     <a href="https://twitter.com/SheCodesInPink" className ="pink_link" target="_blank">Twitter</a>. 
                 </div>
+                <br/>
+                <div className="project_container">
+                    May 29, 2019 
+                    <br/><br/>
+                    Hello World! 
+                    <br/><br/>
+                    Today is the day I officially deployed this site. 
+                    I am very proud of what I was able to create in such a short amount of time.
+                    <br/><br/>
+                    I completed this website in about three several hour sprints, including the conceptualization and design process.
+                    It is a JavaScript/React.js site made without a styling framework.
+                    This site is hosted live from Heroku, which is pulling content from my GitHub.
+                    <br/><br/>
+                    I intend to use this blog to document my job search and my ongoing projects.
+                    <br/><br/>
+                    Cheers,
+                    <br/>
+                    Madison
+                </div>
             </div>
          );
     }
